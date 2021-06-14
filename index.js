3899cfd80961cfe46dcab1fa491688bae2a5dc76
Francis Javier <francisco.izqpax@gmail.com>
2021-06-14 15:13:39 -0500

    make it better

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = 3900;
 mongoose.set('useFindAndModify', false); // desactivate old methods
 mongoose.Promise = global.Promise;
 // conection mongodb
-mongoose.connect('mongodb://localhost:27017/api_rest_blog',{ useNewUrlParser : true, useUnifiedTopology : true }).then( () => {
+mongoose.connect('mongodb+srv://paxtian:paxtian1234@cluster0.bevra.mongodb.net/api_rest_mextilti?retryWrites=true&w=majority',{ useNewUrlParser : true, useUnifiedTopology : true }).then( () => {
     console.log('connection successfull !!');
 
     // create server and listen to htpp request
